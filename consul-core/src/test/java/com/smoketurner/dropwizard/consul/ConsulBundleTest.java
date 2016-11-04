91749e429ad210efdfabd3df768d496de76b04b0
AMeng <alexbmeng@gmail.com>
2016-11-04 11:06:48 -0600

    Add tests for enabled configuration

---
+++ consul-core/src/test/java/com/smoketurner/dropwizard/consul/ConsulBundleTest.java
@@ -0,0 +1,72 @@
+/**
+ * Copyright 2016 Smoke Turner, LLC.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.smoketurner.dropwizard.consul;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.*;
+
+import org.junit.Before;
+import org.junit.Test;
+import io.dropwizard.Configuration;
+import io.dropwizard.setup.Environment;
+
+public class ConsulBundleTest {
+
+    private ConsulBundle bundle;
+    private Configuration config;
+    private final ConsulFactory factory = spy(new ConsulFactory());
+    private final Environment environment = mock(Environment.class);
+
+    @Before
+    public void setUp() throws Exception {
+        class TestConfiguration extends Configuration implements ConsulConfiguration {
+            @Override
+            public ConsulFactory getConsulFactory(Configuration c) {
+                return factory;
+            }
+        }
+
+        bundle = spy(new ConsulBundle<TestConfiguration>("test") {
+            @Override
+            public ConsulFactory getConsulFactory(TestConfiguration c) {
+                return c.getConsulFactory(c);
+            }
+        });
+
+        config = new TestConfiguration();
+        doNothing().when(bundle).runEnabled(factory, environment);
+    }
+
+    @Test
+    public void testDefaultsToEnabled() throws Exception {
+        assertThat(factory.getEnabled()).isTrue();
+    }
+
+    @Test
+    public void testEnabled() throws Exception {
+        doReturn(true).when(factory).getEnabled();
+        bundle.run(config, environment);
+        verify(bundle, times(1)).runEnabled(factory, environment);
+    }
+
+    @Test
+    public void testNotEnabled() throws Exception {
+        doReturn(false).when(factory).getEnabled();
+        bundle.run(config, environment);
+        verify(bundle, times(0)).runEnabled(factory, environment);
+    }
+
+}
