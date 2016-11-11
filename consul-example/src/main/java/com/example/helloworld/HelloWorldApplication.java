d88099121325a37bae05d11f9ddc50df8a8c7797
Justin Plock <jplock@smoketurner.com>
2016-11-11 11:39:00 -0500

    Minor cleanups

--- consul-example/src/main/java/com/example/helloworld/HelloWorldApplication.java
+++ consul-example/src/main/java/com/example/helloworld/HelloWorldApplication.java
@@ -1,3 +1,18 @@
+/**
+ * Copyright 2016 Smoke Turner, LLC.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.example.helloworld;
 
 import com.example.helloworld.resources.HelloWorldResource;
@@ -39,7 +54,7 @@ public class HelloWorldApplication
             Environment environment) throws Exception {
         final Consul consul = configuration.getConsulFactory().build();
         final RibbonJerseyClient loadBalancingClient = new RibbonJerseyClientBuilder(
-                environment, consul, configuration.getDownstream())
+                environment, consul, configuration.getLoadBalancer())
                         .build("hello-world");
 
         final HelloWorldResource resource = new HelloWorldResource(consul,
--- consul-example/src/main/java/com/example/helloworld/HelloWorldConfiguration.java
+++ consul-example/src/main/java/com/example/helloworld/HelloWorldConfiguration.java
@@ -1,13 +1,27 @@
+/**
+ * Copyright 2016 Smoke Turner, LLC.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.example.helloworld;
 
+import javax.validation.Valid;
+import javax.validation.constraints.NotNull;
+import org.hibernate.validator.constraints.NotEmpty;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.smoketurner.dropwizard.consul.ConsulFactory;
 import com.smoketurner.dropwizard.consul.ribbon.RibbonLoadBalancerConfiguration;
 import io.dropwizard.Configuration;
-import org.hibernate.validator.constraints.NotEmpty;
-
-import javax.validation.Valid;
-import javax.validation.constraints.NotNull;
 
 public class HelloWorldConfiguration extends Configuration {
 
@@ -23,7 +37,7 @@ public class HelloWorldConfiguration extends Configuration {
 
     @NotNull
     @Valid
-    public final RibbonLoadBalancerConfiguration downstream = new RibbonLoadBalancerConfiguration();
+    public final RibbonLoadBalancerConfiguration loadBalancer = new RibbonLoadBalancerConfiguration();
 
     @JsonProperty
     public String getTemplate() {
@@ -51,7 +65,7 @@ public class HelloWorldConfiguration extends Configuration {
     }
 
     @JsonProperty
-    public RibbonLoadBalancerConfiguration getDownstream() {
-        return downstream;
+    public RibbonLoadBalancerConfiguration getLoadBalancer() {
+        return loadBalancer;
     }
 }
